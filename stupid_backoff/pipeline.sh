0628ed47339d84cfa4c568f893cffab7cd97812d
joshmorel <morel.josh@hotmail.com>
2016-10-04 04:54:33 -0400

    Added example for pipeline

--- stupid_backoff/pipeline.sh
+++ stupid_backoff/pipeline.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
-INFILE="$(readlink -f ..)/data/examples/examples.txt"
+INFILE="$(readlink -f ..)/example/corpus.txt"
 TASK0=task0_preprocess.py
 TASK1=task1_vocab.py
 TASK2=task2_ngramfreq.py
 TASK3=task3_buildmodel.py
-TASK4=task4_convertRDS.R
-TOKENIZED="$(readlink -f ..)/data/examples/tokenized"
-VOCAB="$(readlink -f ..)/data/examples/_1_vocab"
-NGRAM="$(readlink -f ..)/data/examples/_2_ngram"
-MODEL="$(readlink -f ..)/data/examples/_3_model.txt"
+TOKENIZED="$(readlink -f ..)/example/tokenized.txt"
+VOCAB="$(readlink -f ..)/r/textpred/inst/extdata/vocab.txt"
+NGRAM="$(readlink -f ..)/example/ngrams.txt"
+MODEL="$(readlink -f ..)/r/textpred/inst/extdata/model.txt"
 
-N=3
+N=2
+#the minimum occurrences required for a word to be written to the vocabular
+VOCAB_MIN=2
 
 python3 ${TASK0} ${INFILE} > ${TOKENIZED}
-python3 ${TASK1} ${INFILE} > ${VOCAB}
+python3 ${TASK1} ${TOKENIZED} -vm ${VOCAB_MIN} > ${VOCAB}
 # to ensure sorts as expected
 export LC_ALL=C
 python3 ${TASK2} ${TOKENIZED} ${VOCAB} -n ${N} | sort > ${NGRAM}
-python3 ${TASK3} ${NGRAM} -n ${N} > ${MODEL}
+#-xeos: don't predict end-of-sentence token
+python3 ${TASK3} ${NGRAM} -n ${N} -xeos > ${MODEL}
+
+#clean up unused files
+rm ${TOKENIZED}
+rm ${NGRAM}
